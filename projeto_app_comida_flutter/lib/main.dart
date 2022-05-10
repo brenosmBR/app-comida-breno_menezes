8abc11f43ace953c19e5795e625f141a0e93249f
“Breno <bmenezes_2011@icloud.com>
2022-05-09 22:30:28 -0300

    183_tema e estilo

--- projeto_app_comida_flutter/lib/main.dart
+++ projeto_app_comida_flutter/lib/main.dart
@@ -9,9 +9,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
-      home: CategoriesScreen(),
+      home: const CategoriesScreen(),
     );
   }
 }
